b94c5a29d559a8e62f12979172fb357ad7fcbdfd
Ryan Pham <rtpham10@gmail.com>
2024-05-01 20:00:44 -0700

    Queried information

---
+++ fundamentals.sql
@@ -0,0 +1,9 @@
+-- Question 1
+SELECT email FROM customer
+WHERE first_name = 'Nancy' and last_name = 'Thomas';
+-- Question 2
+SELECT description FROM film
+WHERE title = 'Outlaw Hanky'
+-- Question 3
+SELECT phone FROM address
+WHERE address = '259 Ipoh Drive';
