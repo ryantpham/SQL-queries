d54e1816a0837fe843dc69d46794a17ce8e68f8e
Ryan Pham <rtpham10@gmail.com>
2024-05-03 08:52:01 -0700

    initial commit

--- fundamentals.sql
+++ fundamentals.sql
@@ -5,26 +5,24 @@ SELECT DISTINCT(rating) FROM film;
 SELECT COUNT(DISTINCT(rating)) FROM film;
 
 --SELECT WHERE
--- Q1
 SELECT email FROM customer
 WHERE first_name = 'Nancy' and last_name = 'Thomas';
--- Q2
+
 SELECT description FROM film
 WHERE title = 'Outlaw Hanky'
--- Q3
+
 SELECT phone FROM address
 WHERE address = '259 Ipoh Drive';
 
 --ORDER BY
---Q1
 SELECT customer_id FROM payment
 ORDER BY payment_date ASC
 LIMIT 10;
---Q2
+
 SELECT title,length FROM film
 ORDER BY length ASC
 LIMIT 5;
---Q3
+
 SELECT COUNT(title) FROM film
 WHERE length <= 50;
 
@@ -38,3 +36,19 @@ WHERE amount NOT BETWEEN 8 and 9;
 --IN
 SELECT * FROM customer
 WHERE first_name IN('Jared','Maria','Linda');
+
+--LIKE and ILIKE
+SELECT * FROM customer
+WHERE first_name LIKE'J%' and last_name ILIKE's%';
+
+SELECT * FROM customer
+WHERE first_name LIKE'_her%';
+
+SELECT * FROM customer
+WHERE first_name LIKE'R%'
+ORDER BY last_name ASC;
+
+SELECT * FROM customer
+WHERE first_name LIKE'R%' and last_name NOT LIKE 'B%'
+ORDER BY last_name ASC;
+
