be86e98f4e1bc62c4ab885776a0b20df795243cc
Ryan Pham <rtpham10@gmail.com>
2024-05-04 23:00:45 -0700

    Queried film data to answer business questions using GROUP BY statements

--- groupby.sql
+++ groupby.sql
@@ -20,3 +20,18 @@ SELECT rating,COUNT(release_year) FROM film
 WHERE rating != 'R'
 GROUP BY rating
 ORDER BY COUNT(release_year);
+
+--How many payments did each staff member handle and who gets the bonus
+SELECT staff_id, COUNT(amount) FROM payment
+GROUP BY staff_id;
+
+--What is the average replacement cost per MPAA rating?
+SELECT rating, ROUND(AVG(replacement_cost),2) FROM film
+GROUP BY rating
+ORDER BY rating,AVG(replacement_cost) ASC;
+
+--What are the customer ids of the top 5 customers by total spend?
+SELECT customer_id,SUM(amount) FROM payment
+GROUP BY customer_id
+ORDER BY SUM(amount) DESC
+LIMIT 5;
