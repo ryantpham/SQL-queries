fb8602913bd117548d57a4778807182cf23eafbc
Ryan Pham <rtpham10@gmail.com>
2024-05-05 15:21:45 -0700

    Finished final query

--- assessment-test.sql
+++ assessment-test.sql
@@ -9,3 +9,7 @@ SELECT COUNT(*) FROM film
 WHERE title LIKE'J%';
 
 -- What customer has the highest customer ID number whose name starts with an 'E' and has an address ID lower than 500?
+SELECT first_name,last_name FROM customer
+WHERE first_name LIKE'E%' AND address_id < 500
+ORDER BY customer_id DESC
+LIMIT 1;
