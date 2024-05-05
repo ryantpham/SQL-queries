8d75d7fc9ce133956b60bf5780a2a640254a91dd
Ryan Pham <rtpham10@gmail.com>
2024-05-05 10:49:12 -0700

    Added examples for HAVING statement

--- groupby.sql
+++ groupby.sql
@@ -35,3 +35,15 @@ SELECT customer_id,SUM(amount) FROM payment
 GROUP BY customer_id
 ORDER BY SUM(amount) DESC
 LIMIT 5;
+
+--HAVING
+--The HAVING clause allows us to filter after an aggregation has already taken place.
+SELECT customer_id,COUNT(amount) FROM payment
+GROUP BY customer_id
+HAVING COUNT(amount) >= 40;
+
+--What are the customer ids of customers who have spent more than $100 in payment transactions with our staff_id member 2?
+SELECT customer_id,SUM(amount) FROM payment
+WHERE staff_id = 2
+GROUP BY customer_id
+HAVING SUM(amount) > 100;
