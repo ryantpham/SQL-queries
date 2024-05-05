0836973ee8ab900fb6a8eaad42a1d48da777c88c
Ryan Pham <rtpham10@gmail.com>
2024-05-04 22:24:40 -0700

    Noted some common aggregation functions

--- groupby.sql
+++ groupby.sql
@@ -1,6 +1,15 @@
 --Common Aggregation Functions
+--MIN
 SELECT MIN(replacement_cost) FROM film;
+--MAX
 SELECT MAX(replacement_cost) FROM film;
+--AVG
 SELECT AVG(replacement_cost) FROM film;
+--SUM
 SELECT SUM(replacement_cost) FROM film;
+--COUNT
 SELECT COUNT(replacement_cost) FROM film;
+--ROUND
+SELECT 
+	ROUND(AVG(replacement_cost),3) 
+FROM film;
