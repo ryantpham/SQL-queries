25f45828e19043307366d11dc13e026090f72ccf
Ryan Pham <rtpham10@gmail.com>
2024-05-04 22:18:59 -0700

    initial commit, included some common aggregation functions

---
+++ groupby.sql
@@ -0,0 +1,6 @@
+--Common Aggregation Functions
+SELECT MIN(replacement_cost) FROM film;
+SELECT MAX(replacement_cost) FROM film;
+SELECT AVG(replacement_cost) FROM film;
+SELECT SUM(replacement_cost) FROM film;
+SELECT COUNT(replacement_cost) FROM film;
