d6eadc63b3dd6ffe9a7136d2686e1190bd9d1ee6
Ryan Pham <rtpham10@gmail.com>
2024-05-05 21:09:53 -0700

    added example for INNER JOIN

--- joins.sql
+++ joins.sql
@@ -2,3 +2,9 @@
 SELECT customer_id,SUM(amount) AS total_spent FROM payment
 GROUP BY customer_id
 HAVING SUM(amount) > 200;
+
+--INNER JOIN
+SELECT first_name, payment.customer_id, payment_id 
+FROM payment
+INNER JOIN customer
+ON payment.customer_id = customer.customer_id;
