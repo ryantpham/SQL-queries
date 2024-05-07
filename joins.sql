fbebee158f8e6e4a9a3b2d9c5034eafef8a20030
Ryan Pham <rtpham10@gmail.com>
2024-05-06 21:12:38 -0700

    added example for LEFT OUTER JOIN

--- joins.sql
+++ joins.sql
@@ -16,3 +16,10 @@ ON customer.customer_id = payment.customer_id
 	--Checks where on customer or payment table where customer id happens to be null
 	--WHERE statement finds rows that are only unique to the customer or payment table
 WHERE customer.customer_id IS null OR payment.customer_id IS null;
+
+--LEFT OUTER JOIN
+SELECT film.film_id,title,inventory_id,store_id FROM film
+LEFT OUTER JOIN inventory
+ON inventory.film_id = film.film_id
+	--Checks to see all movies that start with a C that we don't have in inventory
+WHERE title LIKE'C%' AND inventory.film_id IS null;
