a8861b8daf98e59b1b21d85b65f017a3173f9f9b
Ryan Pham <rtpham10@gmail.com>
2024-05-06 20:14:29 -0700

    added example for FULL OUTER JOIN

--- joins.sql
+++ joins.sql
@@ -8,3 +8,11 @@ SELECT first_name, payment.customer_id, payment_id
 FROM payment
 INNER JOIN customer
 ON payment.customer_id = customer.customer_id;
+
+--FULL OUTER JOIN
+SELECT * FROM customer
+FULL OUTER JOIN payment
+ON customer.customer_id = payment.customer_id
+	--Checks where on customer or payment table where customer id happens to be null
+	--WHERE statement finds rows that are only unique to the customer or payment table
+WHERE customer.customer_id IS null OR payment.customer_id IS null;
