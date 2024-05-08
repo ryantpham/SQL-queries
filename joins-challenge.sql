4d40c6747991d3700b4b8dcae316245d9197eb22
Ryan Pham <rtpham10@gmail.com>
2024-05-07 21:20:20 -0700

    completed first query with double FULL OUTER JOIN

--- joins-challenge.sql
+++ joins-challenge.sql
@@ -4,3 +4,22 @@ INNER JOIN address
 ON customer.address_id = address.address_id
 WHERE district IN('California');
 
+--Get a list of all the movies "Nick Wahlberg" has been in.
+
+--Solution 1
+SELECT actor_id FROM actor
+WHERE first_name = 'Nick' AND last_name = 'Wahlberg'
+--OUTPUT: 2
+
+SELECT title,actor_id FROM film
+FULL OUTER JOIN film_actor
+ON film.film_id = film_actor.film_id
+WHERE actor_id = 2
+
+--Solution 2: Double JOIN
+SELECT actor.first_name,actor.last_name,title FROM film
+FULL OUTER JOIN film_actor
+ON film.film_id = film_actor.film_id
+FULL OUTER JOIN actor
+ON actor.actor_id = film_actor.actor_id
+WHERE first_name = 'Nick' AND last_name = 'Wahlberg'
