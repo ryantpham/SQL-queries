d9774641ab8c589cec29a6a105d677d053a9fc35
Ryan Pham <rtpham10@gmail.com>
2024-05-18 01:07:54 -0700

    completed multiple queries

--- assessment-test-2.sql
+++ assessment-test-2.sql
@@ -14,22 +14,35 @@ WHERE membercost > 0;
 -- 4. Produce a list of facilities that charge a fee to members, and that fee is less than 1/50th of the monthly maintenance cost.
 -- Return the facid, facility name, member cost, and monthly maintenance.
 -- Expected Result: 2 rows
+SELECT facid, name, membercost, monthlymaintenance FROM cd.facilities
+WHERE membercost < monthlymaintenance/50 AND membercost > 0;
 
 -- 5. Produce a list of all facilities with the word 'Tennis' in their name.
 -- Expected Result: 3 rows
+SELECT * FROM cd.facilities
+WHERE name ILIKE'%tennis%';
 
 -- 6. Retrieve the details of facilities with ID 1 and 5 without using the OR operator.
 -- Expected Result: 2 rows
+SELECT * FROM cd.facilities
+WHERE facid IN(1,5);
 
 -- 7. Produce a list of members who joined after the start of September 2012.
 -- Return the memid, surname, firstname, and joindate.
 -- Expected Result: 10 rows
+SELECT memid,surname,firstname,joindate FROM cd.members
+WHERE joindate > '2012-09-01';
 
 -- 8. Produce an ordered list of the first 10 surnames in the members table, without duplicates.
 -- Expected Result: 10 rows (including GUEST as a last name)
+SELECT DISTINCT surname FROM cd.members
+ORDER BY surn
 
 -- 9. Retrieve the signup date of the last member.
 -- Expected Result: 2012-09-26 18:08:45
+SELECT DISTINCT joindate FROM cd.members
+ORDER BY joindate DESC
+LIMIT 1;
 
 -- 10. Produce a count of the number of facilities that have a cost to guests of 10 or more.
 -- Expected Result: 6
@@ -48,3 +61,6 @@ WHERE membercost > 0;
 
 -- 14. Produce a list of the start times for bookings by members named 'David Farrell'.
 -- Expected Result: 34 rows
+
+
+--https://docs.google.com/document/d/1wiuYbTQslmfolQWgeVPB356csjK6yqOUBhgC7fM44o8/edit
