85a04d0573f7b92daf467dc226d236164fc40efb
Ryan Pham <rtpham10@gmail.com>
2024-05-18 21:05:33 -0700

    Completed Assessment

--- assessment-test-2.sql
+++ assessment-test-2.sql
@@ -36,31 +36,58 @@ WHERE joindate > '2012-09-01';
 -- 8. Produce an ordered list of the first 10 surnames in the members table, without duplicates.
 -- Expected Result: 10 rows (including GUEST as a last name)
 SELECT DISTINCT surname FROM cd.members
-ORDER BY surn
+ORDER BY surname
+LIMIT 10;
 
 -- 9. Retrieve the signup date of the last member.
 -- Expected Result: 2012-09-26 18:08:45
 SELECT DISTINCT joindate FROM cd.members
 ORDER BY joindate DESC
 LIMIT 1;
+--OR
+SELECT MAX(joindate) AS latest_signup FROM cd.members;
 
 -- 10. Produce a count of the number of facilities that have a cost to guests of 10 or more.
 -- Expected Result: 6
+SELECT COUNT(*) FROM cd.facilities
+WHERE guestcost >= 10;
 
 -- 11. Produce a list of the total number of slots booked per facility in September 2012.
 -- Return facility id and slots, sorted by the number of slots.
 -- Expected Result: 9 rows
+SELECT f.facid, SUM(b.slots) FROM cd.facilities f
+INNER JOIN cd.bookings b
+ON f.facid = b.facid
+WHERE EXTRACT(month FROM starttime) = 9 AND EXTRACT(year FROM starttime) = 2012
+GROUP BY f.facid
+ORDER BY SUM(b.slots);
 
 -- 12. Produce a list of facilities with more than 1000 slots booked.
 -- Return facility id and total slots, sorted by facility id.
 -- Expected Result: 5 rows
+SELECT fac.facid,SUM(slots) as total_slots FROM cd.facilities as fac
+INNER JOIN cd.bookings as book
+ON fac.facid = book.facid
+GROUP BY fac.facid
+HAVING SUM(slots) > 1000
+ORDER BY fac.facid;
 
 -- 13. Produce a list of the start times for bookings for tennis courts on '2012-09-21'.
 -- Return start time and facility name, ordered by time.
 -- Expected Result: 12 rows
+SELECT book.starttime, name FROM cd.facilities as facil
+INNER JOIN cd.bookings as book
+ON facil.facid = book.facid
+WHERE to_char(book.starttime,'YYYY-MM-DD') = '2012-09-21' AND name ILIKE'%Tennis Court%'
+ORDER BY book.starttime ASC;
 
 -- 14. Produce a list of the start times for bookings by members named 'David Farrell'.
 -- Expected Result: 34 rows
-
+SELECT book.starttime,firstname||'-'||surname as full_name 
+FROM cd.bookings as book
+INNER JOIN cd.members as mem
+ON book.memid = mem.memid
+WHERE firstname ILIKE'david' AND surname ILIKE'farrell';
 
 --https://docs.google.com/document/d/1wiuYbTQslmfolQWgeVPB356csjK6yqOUBhgC7fM44o8/edit
+--https://docs.google.com/document/d/1swGZ0RG3KKqWqzmsI_qrMgjJ3lt39mtAJqRSMZy6Z-8/edit
