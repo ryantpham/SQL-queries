a7b4a3f63b5d10a40e361ca9d5cc520b4d0b9a3a
Ryan Pham <rtpham10@gmail.com>
2024-07-18 16:55:22 -0700

    Included misaglignment score to the output

--- cam_blur.sql
+++ cam_blur.sql
@@ -6,7 +6,8 @@ WITH cam_blur AS (
         NULLIF(json_extract_path_text(gr.camera_metadata, 'game_info_v3', 'caseID'), '') AS camera_id,
         SPLIT_PART(COALESCE(g.camera_pi_id, g.pi_id), '/', 1) AS cam_id,
         (json_extract_path_text(gr.camera_metadata, 'blurriness', NULLIF(json_extract_path_text(gr.camera_metadata, 'game_info_v3', 'caseID'), '') || 'A'))::int AS blurry_R,
-        (json_extract_path_text(gr.camera_metadata, 'blurriness', NULLIF(json_extract_path_text(gr.camera_metadata, 'game_info_v3', 'caseID'), '') || 'B'))::int AS blurry_N
+        (json_extract_path_text(gr.camera_metadata, 'blurriness', NULLIF(json_extract_path_text(gr.camera_metadata, 'game_info_v3', 'caseID'), '') || 'B'))::int AS blurry_N,
+        (json_extract_path_text(gr.camera_metadata, 'misalignment', 'raw'))::int AS misalignment_raw
     FROM
         tracedb.game_resources gr
     JOIN
@@ -23,9 +24,10 @@ ranked_games AS (
         cam_id,
         blurry_R,
         blurry_N,
+        misalignment_raw,
         ROW_NUMBER() OVER (
             PARTITION BY COALESCE(camera_id, '')
-            ORDER BY game_date desc -- Most Recent Game
+            ORDER BY game_date DESC -- Most Recent Game
         ) AS row_num
     FROM
         cam_blur
@@ -39,10 +41,12 @@ SELECT
     camera_metadata,
     blurry_R,
     blurry_N,
+    misalignment_raw,
     row_num
 FROM
     ranked_games
 WHERE
-    row_num = 1 OR row_num = 2 --Grabs first 2 Recent games
+    row_num = 1 OR row_num = 2 -- Grabs first 2 Recent games
 ORDER BY
     cam_id, game_date DESC;
+    
